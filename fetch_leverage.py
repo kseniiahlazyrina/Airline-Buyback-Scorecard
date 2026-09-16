076ca79b29278d80d93f5e25778138eaa5d242d4
Kseniia Hlazyrina <kseniiahlazyrina@gmail.com>
2026-09-16 13:49:07 -0700

    Update fetch_leverage.py

--- fetch_leverage.py
+++ fetch_leverage.py
@@ -24,7 +24,7 @@ import requests
 
 # ---------------------------------------------------------------- settings
 
-USER_AGENT = "Karim - UW student project - kseniia@uw.edu"   
+USER_AGENT = "Kseniia - UW student project - kseniia@uw.edu"   
 
 CIKS = {
     "DAL":  "0000027904",
